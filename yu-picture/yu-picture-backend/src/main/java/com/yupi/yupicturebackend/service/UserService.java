80fd0081cf9e65e15b18d22f03fa5d49a8664e9d
孔德丞 <2480155857@qq.com>
2025-04-12 14:54:40 +0800

    feat(user): 添加用户相关实体类和接口
    - 新增 User 实体类，包含用户相关属性- 新增 UserMapper 接口，用于用户表的数据库操作- 新增 UserService 接口和 UserServiceImpl 实现类，用于用户相关的业务逻辑处理
    - 新增 UserMapper.xml 配置文件，定义用户表的 MyBatis映射

---
+++ yu-picture/yu-picture-backend/src/main/java/com/yupi/yupicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author kdc
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-04-12 14:46:37
+*/
+public interface UserService extends IService<User> {
+
+}
