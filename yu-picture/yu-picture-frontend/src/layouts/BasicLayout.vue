a75c66a15da393d7f88b7b0b89ec6961044bd7a6
孔德丞 <2480155857@qq.com>
2025-04-12 14:53:09 +0800

    refactor(yu-picture): 重构项目配置和布局
    - 修改数据库连接 URL，从 yu_picture切换到 picture
    - 更新前端布局中的版权链接和文本
    - 简化主页欢迎消息

--- yu-picture/yu-picture-frontend/src/layouts/BasicLayout.vue
+++ yu-picture/yu-picture-frontend/src/layouts/BasicLayout.vue
@@ -8,7 +8,7 @@
         <router-view />
       </a-layout-content>
       <a-layout-footer class="footer">
-        <a href="https://www.codefather.cn" target="_blank"> 编程导航 by 程序员鱼皮 </a>
+        <a href="https://kdcfff.github.io" target="_blank"> kdc </a>
       </a-layout-footer>
     </a-layout>
   </div>
